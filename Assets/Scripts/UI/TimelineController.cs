9d3229415159d2e62761fc66eb347ad95f18db69
MitchFire48 <90575206+MitchFire48@users.noreply.github.com>
2021-10-09 15:14:19 -0500

    Update TimelineController.cs

--- Assets/Scripts/UI/TimelineController.cs
+++ Assets/Scripts/UI/TimelineController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+// test 2
 public class TimelineController : MonoBehaviour
 {
     [SerializeField] Timeline m_currentTime = Timeline.Present;
