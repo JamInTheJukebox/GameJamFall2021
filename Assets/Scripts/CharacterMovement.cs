959f06690394b36818c23bb845162b3c1c2823b9
MitchFire48 <90575206+MitchFire48@users.noreply.github.com>
2021-10-21 14:51:12 -0500

    Player faces correct direction. Level 2 framework. Knife image added.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    private SpriteRenderer mySpriteRenderer;
     public float horizontalSpeed = 8;
     Rigidbody2D rb;
-
     // Awake is called as soon as player presses play
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,5 +20,20 @@ public class CharacterMovement : MonoBehaviour
     {
         var vel = Input.GetAxis("Horizontal") * horizontalSpeed;
         rb.velocity = new Vector2(vel, rb.velocity.y);
+        // if the A key was pressed this frame
+        if (vel < 0)
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+        }
+        if (vel > 0)
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = false;
+            }
+        }
     }
 }
