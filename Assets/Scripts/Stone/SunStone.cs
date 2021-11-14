4f7ba895f827922aea317276569c10777b269bb8
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2021-11-14 04:11:21 -0600

    Crowd

--- Assets/Scripts/Stone/SunStone.cs
+++ Assets/Scripts/Stone/SunStone.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal; //2019 VERSIONS
 
-public class MoonStone : StoneBehavior
+public class SunStone : StoneBehavior
 {
-    public Light2D MoonLight;
+    public Light2D SunStoneLight;
 
     public float NewIntensity;
     private float OldIntensity;
@@ -14,16 +14,19 @@ public class MoonStone : StoneBehavior
     private void Awake()
     {
         InitializeAnimator();
-        OldIntensity = MoonLight.intensity;
+
     }
 
     public override float UseStone()
     {
-        if (stateChanged)
-            MoonLight.intensity = OldIntensity;     // handle any changes here to sprites
-        else
-            MoonLight.intensity = NewIntensity;
-        stateChanged = !stateChanged;
         return base.UseStone();         // return the cooldown time;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UseStone();
+        }
     }
 }
